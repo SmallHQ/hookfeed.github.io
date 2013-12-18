37fc11278012a129393ea2305fc59f693e76fa1a
Joelle Steiniger <joelle@smallhq.com>
2013-12-17 17:35:58 -0800

    updated home page

---
+++ source/javascripts/home.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  window.setTimeout(function(){
+    $('[data-typer-targets]').typer({
+      highlightSpeed    : 400,
+      typeSpeed         : 50,
+      clearDelay        : 200,
+      typeDelay         : 100,
+      clearOnHighlight  : true,
+      typerInterval     : 4000
+    });
+  }, 4000);
+});
