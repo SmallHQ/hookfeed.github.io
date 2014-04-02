e70aa59034b26981206c897c8472e41a7740e3a5
Matt Goldman <matt@smallhq.com>
2014-04-01 22:35:35 -0700

    finished up frontend changes

--- source/javascripts/home.js
+++ source/javascripts/home.js
@@ -2,33 +2,12 @@ var stripe_coupon_id = $.url().param('c');
 
 $(document).ready(function(){
   if(stripe_coupon_id)
-    $("a.signup:not('#0_0_20_btn')").each(function() {
+    $("a.signup").each(function() {
       var _href = $(this).attr("href");
-      $(this).attr("href", _href + '&c=' + stripe_coupon_id);
+      $(this).attr("href", _href + '&c=' + stripe_coupon_id.replace('/', ''));
+
+      $(this).append('<span>and redeem coupon</span>');
     });
-
-  /* On Page Load (i.e. back button) */
-  $('input[type="checkbox"]', 'form.annual_offer').each(function(){
-    updatePricingFor($(this));
-  });
-
-  /* On Checked/Unchecked */
-  $('input[type="checkbox"]', 'form.annual_offer').change(function(){
-    updatePricingFor($(this));
-  });
-
-  // Lookup charges
-  // Set time to 30 days ago if we have JS and fade in button
-  $('a', '#charge_instructions').attr('href', 'https://manage.stripe.com/payments?created%5Bgte%5D=' + Math.floor((30).days().ago().getTime()/1000));
-  $('#lookup_charges').show();
-
-  $('#lookup_charges').click(function(){
-    $(this).fadeOut(300, function(){
-      $('#charge_instructions').fadeIn(300);
-    });
-
-    return false;
-  });
 
   // Sample Emails
   $('a', '#stripe_events').click(function(){
@@ -45,43 +24,3 @@ $(document).ready(function(){
     return false;
   });
 });
-
-function updatePricingFor($checkbox){
-  if ($checkbox.is(':checked')) {
-    switchToYearlyPricing($checkbox);
-    yearlySignupButton($checkbox);
-  } else {
-    switchToMonthlyPricing($checkbox);
-    monthlySignupButton($checkbox);
-  }
-}
-
-function switchToMonthlyPricing($checkbox){
-  $checkbox.parent().parent().siblings('.yearly_price').hide();
-  $checkbox.parent().parent().siblings('.monthly_price').show();
-}
-
-function switchToYearlyPricing($checkbox){
-  $checkbox.parent().parent().siblings('.monthly_price').hide();
-  $checkbox.parent().parent().siblings('.yearly_price').show();
-}
-
-function yearlySignupButton($checkbox){
-  $button = $checkbox.parent().parent().siblings('a.signup');
-
-  if(stripe_coupon_id){
-    $button.attr('href', $button.attr('href').split('?')[0] + '?p=' + $checkbox.data('yearly-plan-id') + '&c=' + stripe_coupon_id);
-  }else{
-    $button.attr('href', $button.attr('href').split('?')[0] + '?p=' + $checkbox.data('yearly-plan-id'));
-  }
-}
-
-function monthlySignupButton($checkbox){
-  $button = $checkbox.parent().parent().siblings('a.signup');
-
-  if(stripe_coupon_id){
-    $button.attr('href', $button.attr('href').split('?')[0] + '?p=' + $button.data('monthly-plan-id') + '&c=' + stripe_coupon_id);
-  }else{
-    $button.attr('href', $button.attr('href').split('?')[0] + '?p=' + $button.data('monthly-plan-id'));
-  }
-}
