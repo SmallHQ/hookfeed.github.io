006b23c6c394c104f4fb0a7be84060f5215649d9
Matt Goldman <matt@smallhq.com>
2014-04-08 09:38:34 -0700

    fixed up image section

--- source/javascripts/home.js
+++ source/javascripts/home.js
@@ -9,13 +9,28 @@ $(document).ready(function(){
       $(this).append('<span>and redeem coupon</span>');
     });
 
+  // Set initial height of email preview
+  if($('body').width() >= 960 ){
+    $('#email_preview').css('height', 667);
+  }else{
+    $('#email_preview').css('height', 574);
+  }
+
   // Sample Emails
   $('a', '#stripe_events').click(function(){
     $link = $(this);
 
     // Change Active Email Preview
-    $('#email_preview').attr('height', $link.data('height'));
-    $('img', '#email_preview').attr('src', $link.data('src'));
+    if($('body').width() >= 960 ){
+      width = $link.data('height');
+    }else{
+      width = $link.data('responsive-height');
+    }
+
+    $('#email_preview').css('height', width);
+    $('body')
+    $('img', '#email_preview').removeClass('active');
+    $('img#' + $link.data('img'), '#email_preview').addClass('active');
 
     // Change Active Stripe Event
     $('a', '#stripe_events').removeClass('active');
