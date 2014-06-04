92f52a4b07bcc02ce365259607bb66a17a1f23a2
Matt Goldman <matt@smallhq.com>
2014-06-04 08:56:00 -0700

    Automated commit at 2014-06-04 15:56:00 UTC by middleman-deploy 0.1.3

--- javascripts/interval_switcher-a6dbdf80.js
+++
@@ -1,1 +0,0 @@
-$(document).ready(function(){$("#toggle_monthly").click(function(){return $(this).addClass("active"),$("#toggle_annual").removeClass("active"),!1}),$("#toggle_annual").click(function(){return $(this).addClass("active"),$("#toggle_monthly").removeClass("active"),!1})});
