6eece712f1b56c198f3401d6b2aee333adebec15
Matt Goldman <matt@smallhq.com>
2014-06-04 00:39:25 -0700

    fixed up JS, good to go.

--- source/javascripts/pricing.js
+++ source/javascripts/pricing.js
@@ -1,3 +1,5 @@
+//= require_tree .
+
 Number.prototype.format = function(n, x) {
   var re = '\\d(?=(\\d{' + (x || 3) + '})+' + (n > 0 ? '\\.' : '$') + ')';
   return this.toFixed(Math.max(0, ~~n)).replace(new RegExp(re, 'g'), '$&,');
