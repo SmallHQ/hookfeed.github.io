04cb6135032369b7b4f0239b838b7537baf2c8d6
Matt Goldman <matt@smallhq.com>
2014-06-03 20:22:56 -0700

    started on pricing

--- source/javascripts/_pricing.js
+++ source/javascripts/_pricing.js
@@ -1,43 +1,82 @@
-var pricing_plans = $.url().param('p');
+Number.prototype.format = function(n, x) {
+  var re = '\\d(?=(\\d{' + (x || 3) + '})+' + (n > 0 ? '\\.' : '$') + ')';
+  return this.toFixed(Math.max(0, ~~n)).replace(new RegExp(re, 'g'), '$&,');
+};
+
+var pricing_plans = $.url().param('p') || "m_39900_10_1501_3000,m_24900_5_501_1500,m_12900_3_101_500,m_3900_2_26_100,m_0_1_0_25";
+var pricing_plans_array = pricing_plans.split(',');
+var plan1_parts = pricing_plans_array[0].split('_');
+var plan2_parts = pricing_plans_array[1].split('_');
+var plan3_parts = pricing_plans_array[2].split('_');
+var plan4_parts = pricing_plans_array[3].split('_');
+var free_plan_parts = pricing_plans_array[0].split('_');
+
+var plan1 = {
+  plan_id: pricing_plans_array[0],
+  monthly_amount: "$" + (parseInt(plan1_parts[1])/100).format(),
+  yearly_amount: "$" + (parseInt(plan1_parts[1] * 10)/100).format(),
+  customers_limit: parseInt(plan1_parts[4]),
+  teammates_limit: parseInt(plan1_parts[2])
+}
+var plan2 = {
+  plan_id: pricing_plans_array[1],
+  monthly_amount: "$" + (parseInt(plan2_parts[1])/100).format(),
+  yearly_amount: "$" + (parseInt(plan2_parts[1] * 10)/100).format(),
+  customers_limit: parseInt(plan2_parts[4]),
+  teammates_limit: parseInt(plan2_parts[2])
+}
+var plan3 = {
+  plan_id: pricing_plans_array[2],
+  monthly_amount: "$" + (parseInt(plan3_parts[1])/100).format(),
+  yearly_amount: "$" + (parseInt(plan3_parts[1] * 10)/100).format(),
+  customers_limit: parseInt(plan3_parts[4]),
+  teammates_limit: parseInt(plan3_parts[2])
+}
+var plan4 = {
+  plan_id: pricing_plans_array[3],
+  monthly_amount: "$" + (parseInt(plan4_parts[1])/100).format(),
+  yearly_amount: "$" + (parseInt(plan4_parts[1] * 10)/100).format(),
+  customers_limit: parseInt(plan4_parts[4]),
+  teammates_limit: parseInt(plan4_parts[2])
+}
+var free_plan = {
+  plan_id: pricing_plans_array[4],
+  monthly_amount: "$" + (parseInt(free_plan_parts[1])/100).format(),
+  yearly_amount: "$" + (parseInt(free_plan_parts[1] * 10)/100).format(),
+  customers_limit: parseInt(free_plan_parts[4]),
+  teammates_limit: parseInt(free_plan_parts[2])
+}
 
 $(document).ready(function() {
+  toggleInterval(false); // set to monthly prices on load
+
   $('input', '#annual_toggle').change(function(){
     $(this).parent().toggleClass('active');
+    toggleInterval(this.checked);
   });
 
-  if(!pricing_plans){
-    pricing_plans = "m_0_1_0_25,m_3900_2_26_100,m_12900_3_101_500,m_24900_5_501_1500,m_39900_10_1501_3000";
-  }
-
-  plan1 = new Plan(pricing_plans[0]);
-  plan2 = new Plan(pricing_plans[1]);
-  plan3 = new Plan(pricing_plans[2]);
-  plan4 = new Plan(pricing_plans[3]);
-
-  plans = [
-    {
-      "name": "Starter",
-      "customers_limit": plan1.customers_limit(),
-      "teammates_limit": plan1.teammates_limit(),
-      "monthly_price": plan1.monthly_price(),
-      "yearly_price": plan1.yearly_price()
-    }
-  ]
-
+  console.log(plan1);
+  console.log(plan2);
+  console.log(plan3);
+  console.log(plan4);
 });
 
-function Plan(plan_id){
-  self.plan_id = plan_id;
-}
+function toggleInterval(checked){
+  $('.pricing_bucket').each(function(index, element){
+    $this = $(this);
+    $inner = $this.children('.inner');
+    plan = eval('plan' + (index+1));
 
-Plan.prototype.customers_limit = function(){
-  return self.plan_id.split
-}
+    if(checked){
+      // Yearly
+      price = plan.yearly_amount + "<span>/year</span>"
+    }else{
+      //Monthly
+      price = plan.monthly_amount + "<span>/month</span>"
+    }
 
-function customers_limit(plan_number) {
-  pricing_plans[plan_number].split('_')
-}
-
-function interval(plan_number) {
-
+    $inner.children('h3').html(price);
+    $inner.children('ul').children('li.teammates_limit').html("<span>" + plan.teammates_limit + "</span><strong>Teammates</strong>");
+    $inner.children('ul').children('li.customers_limit').html("<span>" + plan.customers_limit + "</span><strong>Paying Customers</strong>");
+  });
 }
