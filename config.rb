1337073e3d7a26eb29207f3ab7eb4a2da8c3a309
Matt Goldman <matt@smallhq.com>
2014-08-28 22:30:26 -0700

    consolidating html layouts - still need to clean up scss

--- config.rb
+++ config.rb
@@ -44,47 +44,19 @@ activate :deploy do |deploy|
   deploy.branch = "gh-pages"
 end
 
-set :css_dir, 'stylesheets'
-set :js_dir, 'javascripts'
-set :images_dir, 'images'
+set :css_dir, 'assets/scss'
+set :js_dir, 'assets/js'
 
 set :markdown_engine, :redcarpet
 set :markdown, fenced_code_blocks: true, smartypants: true
 
-###
-# Page options, layouts, aliases and proxies
-###
-
-# Per-page layout changes:
-#
-# With no layout
-# page "/path/to/file.html", :layout => false
-#
-# With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
-#
-# A path which all have the same layout
-# with_layout :admin do
-#   page "/admin/*"
-# end
 page "/feed.xml", layout: false
 page "/sitemap.xml", layout: false
 page "/robots.txt", layout: false
 
 # Build-specific configuration
 configure :build do
-  # For example, change the Compass output style for deployment
   activate :minify_css
-
-  # Minify Javascript on build
   activate :minify_javascript
-
-  # Enable cache buster
   activate :asset_hash
-
-  # Use relative URLs
-  # activate :relative_assets
-
-  # Or use a different image path
-  # set :http_prefix, "/Content/images/"
 end
